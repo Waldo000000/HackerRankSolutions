c130f9e92b4f6e03ea49cd116660e608176f9aac
Roy Wallace <waldo000000@hotmail.com>
2016-10-30 11:04:14 +1000

    Array left rotation

---
+++ Solutions.Tests/LeftRotationTest.cs
@@ -0,0 +1,37 @@
+﻿using EasyAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Solutions.Tests
+{
+    [TestClass]
+    public class LeftRotationTest
+    {
+        [TestMethod]
+        public void LeftRotate_LessThanArrayLength_RotatesElements()
+        {
+            LeftRotation
+                .LeftRotate(new[] { 1, 2, 3, 4, 5 }, 4)
+                .ShouldMatch(5, 1, 2, 3, 4);
+
+            LeftRotation
+                .LeftRotate(new[] { 1, 2, 3, 4, 5 }, 2)
+                .ShouldMatch(3, 4, 5, 1, 2);
+        }
+
+        [TestMethod]
+        public void LeftRotate_ArrayLength_NoChange()
+        {
+            LeftRotation
+                .LeftRotate(new[] { 1, 2, 3, 4, 5 }, 5)
+                .ShouldMatch(1, 2, 3, 4, 5);
+        }
+
+        [TestMethod]
+        public void LeftRotate_MoreThanArrayLength_RotatesElements()
+        {
+            LeftRotation
+                .LeftRotate(new[] { 1, 2, 3, 4, 5 }, 6)
+                .ShouldMatch(2, 3, 4, 5, 1);
+        }
+    }
+}
---
+++ Solutions/LeftRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+namespace Solutions
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/ctci-array-left-rotation
+    /// Given an array of integers and a number, , perform  left rotations on the array. Then print the updated array as a single line of space-separated integers.
+    /// </summary>
+    public class LeftRotation
+    {
+        public static int[] LeftRotate(int[] array, int numLeftRotations)
+        {
+            return Enumerable.Range(0, array.Length)
+                .Select(i => (i + numLeftRotations) % array.Length)
+                .Select(i => array[i]).ToArray();
+        }
+    }
+}

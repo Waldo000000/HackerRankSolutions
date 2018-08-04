f3fc92e269bb2a5e90c5f3bab257749a735f9366
Roy Wallace <waldo000000@gmail.com>
2018-08-04 11:53:45 +1000

    Some more test cases (TDD)

--- Solutions.Tests/TimeInWordsTest.cs
+++ Solutions.Tests/TimeInWordsTest.cs
@@ -6,7 +6,21 @@ namespace Solutions.Tests
     class TimeInWordsTest
     {
         [TestCase(5, 0, ExpectedResult = "five o'clock")]
+        [TestCase(12, 0, ExpectedResult = "twelve o'clock")]
         public string OnTheHour_ReturnsOclock(int hours, int minutes)
+        {
+            return TimeInWords.ConvertTimeToWords(hours, minutes);
+        }
+
+        [TestCase(5, 1, ExpectedResult = "one minute past five")]
+        [TestCase(5, 10, ExpectedResult = "ten minutes past five")]
+        public string LessThanThirtyMinutes_MinutesPastTheHour(int hours, int minutes)
+        {
+            return TimeInWords.ConvertTimeToWords(hours, minutes);
+        }
+
+        [TestCase(5, 15, ExpectedResult = "quarter past five")]
+        public string FifteenMinutes_QuarterPast(int hours, int minutes)
         {
             return TimeInWords.ConvertTimeToWords(hours, minutes);
         }
--- Solutions/TimeInWords.cs
+++ Solutions/TimeInWords.cs
@@ -2,13 +2,56 @@
 {
     public class TimeInWords
     {
+        private static readonly string[] Units =
+        {
+            "zero",
+            "one",
+            "two",
+            "three",
+            "four",
+            "five",
+            "six",
+            "seven",
+            "eight",
+            "nine",
+            "ten",
+            "eleven",
+            "twelve",
+            "thirteen",
+            "fourteen",
+            "fifteen",
+            "sixteen",
+            "seventeen",
+            "eighteen",
+            "nineteen"
+        };
+
         /// <summary>
         /// Given the time in numerals, convert it into words
         /// https://www.hackerrank.com/challenges/the-time-in-words/problem
         /// </summary>
         public static string ConvertTimeToWords(int hour, int minute)
         {
-            return "five o'clock";
+            string hourAmount = Units[hour];
+            if (minute == 0)
+            {
+                return $"{hourAmount} o'clock";
+            }
+
+            if (minute == 15)
+            {
+                return $"quarter past {hourAmount}";
+            }
+
+            string minuteStringSuffix = GetMinuteStringSuffix(minute);
+            return $"{Units[minute]} {minuteStringSuffix} past {hourAmount}";
+        }
+
+        private static string GetMinuteStringSuffix(int minute)
+        {
+            return minute == 1
+                ? "minute"
+                : "minutes";
         }
     }
 }

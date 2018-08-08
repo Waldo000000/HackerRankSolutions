f45a399d7c4bdf443f4b2d12bf33fae95b865397
Roy Wallace <waldo000000@gmail.com>
2018-08-08 21:01:32 +1000

    Algorithms/Strings/CamelCase

---
+++ Solutions.Tests/Algorithms/Strings/CamelCaseTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using Solutions.Algorithms.Strings;
+
+namespace Solutions.Tests.Algorithms.Strings
+{
+    [TestFixture]
+    public class CamelCaseTest
+    {
+        [TestCase("a", ExpectedResult = 1)]
+        [TestCase("foo", ExpectedResult = 1)]
+        [TestCase("fooBar", ExpectedResult = 2)]
+        [TestCase("saveChangesInTheEditor", ExpectedResult = 5)]
+        public int CamelCase_SomeString_CountsWords(string input)
+        {
+            return CamelCase.Camelcase(input);
+        }
+    }
+}
---
+++ Solutions/Algorithms/Strings/CamelCase.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Solutions.Algorithms.Strings
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/camelcase/problem
+    /// </summary>
+    public class CamelCase
+    {
+        public static int Camelcase(string s)
+        {
+            return Regex.Matches(s, "(^[A-Za-z]|[A-Z])[a-z]*").Count;
+        }
+    }
+}

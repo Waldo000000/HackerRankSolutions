ad4ce2d6b6d2e7c2b912ec9f4244a468ed2ad6a9
Roy Wallace <waldo000000@gmail.com>
2018-08-08 21:54:55 +1000

    DataStructures/LinkedLists/CycleDetection

---
+++ Solutions/DataStructures/LinkedLists/CycleDetection.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace Solutions.DataStructures.LinkedLists
+{
+    public class SinglyLinkedListNode
+    {
+        // ReSharper disable once InconsistentNaming
+        public int data;
+        // ReSharper disable once InconsistentNaming
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData)
+        {
+            data = nodeData;
+            next = null;
+        }
+    }
+
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/detect-whether-a-linked-list-contains-a-cycle/problem
+    /// </summary>
+    public class CycleDetection
+    {
+        public static bool HasCycle(SinglyLinkedListNode head)
+        {
+            var nodes = new HashSet<SinglyLinkedListNode>();
+            do
+            {
+                if (nodes.Contains(head))
+                {
+                    return true;
+                }
+
+                nodes.Add(head);
+                head = head.next;
+            } while (head != null);
+
+            return false;
+        }
+    }
+}

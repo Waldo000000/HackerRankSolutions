cffe961f24168b981d450919a3339e8c99137d3e
Roy Wallace <waldo000000@gmail.com>
2018-08-11 19:37:34 +1000

    Algorithms/Strings/Gemstones

---
+++ Solutions.Tests/Algorithms/Strings/GemstonesTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Solutions.Algorithms.Strings;
+
+namespace Solutions.Tests.Algorithms.Strings
+{
+    [TestFixture]
+    public class GemstonesTest
+    {
+        [TestCase("abcdde","baccd","eeabg", ExpectedResult = 2)]
+        [TestCase("a", "a", ExpectedResult = 1)]
+        [TestCase("ab", "a", ExpectedResult = 1)]
+        [TestCase("ab", "ab", ExpectedResult = 2)]
+        [TestCase("fjdkl","fdjoii", "fdksj", "afjsid", ExpectedResult = 3)]
+        public int Gemstones_SomeMineralLists_CountsNumMineralsThatAppearInAllRocks(params string [] compositions)
+        {
+            return Gemstones.CountGemstones(compositions);
+        }
+    }
+}
---
+++ Solutions/Algorithms/Strings/Gemstones.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Solutions.Algorithms.Strings
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/gem-stones/problem
+    /// </summary>
+    public class Gemstones
+    {
+        public static int CountGemstones(string[] compositions)
+        {
+            HashSet<char> minerals = new HashSet<char>(compositions.SelectMany(c => c.ToCharArray()));
+
+            IList<HashSet<char>> compositionSets = compositions
+                .Select(composition => new HashSet<char>(composition.ToCharArray()))
+                .ToList();
+
+            return minerals.Count(m => compositionSets.All(c => c.Contains(m)));
+        }
+    }
+}

c8b4726ae75529b0f54f1be1df40e51e6f0b09fc
Roy Wallace <waldo000000@gmail.com>
2018-08-11 12:43:59 +1000

    WIP: Jim and the Skyscrapers

---
+++ Solutions.Tests/DataStructures/Advanced/JimAndTheSkyscrapersTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using Solutions.DataStructures.Advanced;
+
+namespace Solutions.Tests.DataStructures.Advanced
+{
+    [TestFixture]
+    public class JimAndTheSkyscrapersTest
+    {
+        [TestCase(new[] {3, 2, 1, 2, 3, 3}, ExpectedResult = 8)]
+        [TestCase(new[] {1, 1000, 1}, ExpectedResult = 0)]
+        public int Solve_ForAListOfHeights_ReturnsNumberOfPossibleDirectRoutes(int[] heights)
+        {
+            return JimAndTheSkyscrapers.Solve(heights);
+        }
+    }
+}
---
+++ Solutions/DataStructures/Advanced/JimAndTheSkyscrapers.cs
@@ -0,0 +1,13 @@
+﻿namespace Solutions.DataStructures.Advanced
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/jim-and-the-skyscrapers/problem
+    /// </summary>
+    public class JimAndTheSkyscrapers
+    {
+        public static int Solve(int[] heights)
+        {
+            return 0;
+        }
+    }
+}
